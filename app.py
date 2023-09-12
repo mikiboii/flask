3d83b2182ea17f9be85a5e5bd4d004575e6d9e25
mikiboii <mikiboii21@gmail.com>
2023-09-12 04:17:26 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+from livereload import Server
+
+# livereload-2.4.0
+
+app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+@app.route('/')
+def index():
+    # return "cc"
+    return render_template('mm.html')
+
+@app.route('/bart')
+def use_jinja():
+    return render_template('mm.html')
+
+
+
+if __name__ == '__main__':
+    server = Server(app.wsgi_app)
+    server.serve(port=5000)
+    # app.run(port=5000,debug=True, extra_files=['/static'])
+  
