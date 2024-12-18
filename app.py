58aa1317f2ac37a5b81698e2c309fb408e087927
mikiboii <121342214+mikiboii@users.noreply.github.com>
2024-12-18 12:07:08 -0800

    hh

--- app.py
+++ app.py
@@ -1,16 +1,57 @@
-from flask import Flask, render_template
-from livereload import Server
-import os
-# livereload-2.4.0
+from flask import Flask
+import threading
 
+
+
+import os
+import subprocess
+import time
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
-print("server starting...")
+        # Live stream URL
+url = "https://pull-f5-tt03.tiktokcdn.com/game/stream-3286932347346486149_sd.flv?_session_id=053-2024121721004213676F5D2466123F4F24.1734469240661&expire=1735678843&sign=4e853f98a54e52283a1e10b752cee3f9&_webnoredir=1&abr_pts=810466"
 
-print(os.system("ls"))
+        # Twitch RTMP URL
+twitch_rtmp_url = "rtmp://live-lax.twitch.tv/app/live_1072101235_ztWGwxq7oMHGHkVmsrbqDIGvTV5DW2"
 
-print(os.system("ffmpeg"))
+def miki():
+
+    # while True:
+
+
+        # Duration to stream in seconds
+        stream_duration = 60  # Stream for 1 minute
+
+        start_time = time.time()
+
+        try:
+            # Use FFmpeg to stream the live stream to the Twitch RTMP URL
+            subprocess.run([
+                "ffmpeg",
+                "-i", url,
+                "-c", "copy",
+                "-f", "flv",
+                "-fflags", "nobuffer",
+                "-flags", "low_delay",
+                twitch_rtmp_url
+            ], check=True)
+
+        except subprocess.CalledProcessError as e:
+            print(f"Error streaming live stream: {e}")
+
+        except KeyboardInterrupt:
+            print("Stream interrupted by user")
+
+t1 = threading.Thread(target=miki)
+t1.start()
+
+
+# print("server starting...")
+
+# print(os.system("ls"))
+
+# print(os.system("ffmpeg"))
 
 @app.route('/')
 def index():
