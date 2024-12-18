73c0080d91719c8b2d56ae350bc6d304f6099364
mikiboii <121342214+mikiboii@users.noreply.github.com>
2024-12-18 11:51:48 -0800

    ff

--- app.py
+++ app.py
@@ -21,5 +21,5 @@ if __name__ == '__main__':
     # server = Server(app.wsgi_app)
     # server.serve(port=5000)
     print("server starting...")
-    app.run()
+    app.run(host="0.0.0.0", port=10000)
   
