50819d3d7ef352874f1d4a58b103f39c32d74ac4
mikiboii <121342214+mikiboii@users.noreply.github.com>
2024-12-18 22:56:36 +0000

    ss

--- del.py
+++ del.py
@@ -1,8 +1,10 @@
 import requests    
     
-try:
-    response = requests.get('https://flask-ap18.onrender.com/')
-    print(f"Response status code from miki_test: {response.status_code}")
-    # print(f"Response content: {response.content.decode()}")
-except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
+# try:
+#     response = requests.get('https://flask-ap18.onrender.com/')
+#     print(f"Response status code from miki_test: {response.status_code}")
+#     # print(f"Response content: {response.content.decode()}")
+# except requests.exceptions.RequestException as e:
+#         print(f"Error: {e}")
+
+
